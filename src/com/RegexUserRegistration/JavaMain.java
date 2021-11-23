212092eed4e9a8faac102ad9dd4bc9ade738c2aa
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-23 14:59:00 +0530

    ADDED :  emailid pattern of regex

---
+++ src/com/RegexUserRegistration/JavaMain.java
@@ -0,0 +1,27 @@
+package com.RegexUserRegistration;
+
+import java.util.Scanner;
+
+public class JavaMain {
+	public static void main(String[] args) {
+		JavaRegex regex = new JavaRegex();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the choice 1.firstname 2.lastname 3.emailid:");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			regex.firstName();
+			break;
+		case 2:
+			regex.lastName();
+			break;
+		case 3:
+			regex.emailId();
+			break;
+
+		default:
+			System.out.println("Enter the valid data");
+			break;
+		}
+	}
+}
